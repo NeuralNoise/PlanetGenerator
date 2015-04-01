7ca217d0a28b0b51851e3ede91e5dabb04a7b2be
AdrienGannerie <adrien.gannerie@gmail.com>
2015-04-01 12:09:03 +0200

    AjoutHeightGenerator

---
+++ Assets/Scripts/Terrain_Generator/HeightGenerator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeightGenerator
+{
+	private int size = 250;
+	private byte[,] biomeGenerator(byte[,] temperature,byte[,] humidity)
+	{
+		byte[,] l_biome = new byte[250,250];
+		return l_biome;
+	}
+
+	private byte[,] temperatureGenerator(int size)
+	{
+		byte[,] l_temperature = new byte[size,size];
+		return l_temperature;
+	}
+
+	private byte[,] humidityGenerator(byte[,] l_humidityBord)
+	{
+		byte[,] l_humidity = l_humidityBord;
+		for(int i=0;i<l_humidity.Length;i++)
+		{
+			for(int p=0;p<l_humidity.Length;p++)
+			{
+				if(l_humidity[i,p] == 0)
+				{
+					int l_someHeight()
+				}
+			}
+		}
+		return l_humidity;
+	}
+}
